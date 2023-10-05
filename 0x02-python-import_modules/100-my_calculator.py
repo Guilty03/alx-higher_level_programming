5c40d1759dacd10dd9919656c7e998486b921ac8
Innocent <innocent04102003@gmail.com>
2023-10-05 03:20:45 -0700

    Python - import & modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,23 @@
 
 if __name__ == "__main__":
     """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
 
-    if len(sys.argv) - 1 != 3:
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
     ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+
+    if operator not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    result = ops[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
