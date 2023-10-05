835b6b8a2c7bd52f0aab6a3e536b40ed3a3fbf06
Innocent <innocent04102003@gmail.com>
2023-10-05 03:35:28 -0700

    Python - import & modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,22 +1,15 @@
-from magic_calculation_102 import add, sub
+#!/usr/bin/python3
+
 
 def magic_calculation(a, b):
-    add_result = add(a, b)
-    
-    if add_result < 0:
-        return add_result
+    """Match bytecode provided by Holberton School."""
+    from magic_calculation_102 import add, sub
 
-    sub_result = sub(a, b)
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
 
-    for i in range(4, 6):
-        add_result = add(add_result, i)
-
-    return add_result
-
-if __name__ == "__main__":
-    a = 10  # Example values for testing
-    b = 5   # Modify as needed
-    result = magic_calculation(a, b)
-
-    if result is not None:
-        print(f"Result: {result}")
+    else:
+        return(sub(a, b))
