274072441917bda71fbc5d7034472355ce5d21fb
Innocent <innocent04102003@gmail.com>
2023-10-24 00:56:41 -0700

    Python - Exceptions

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    '''Performs calculations between two integers
+    Args:
+        a: The first integer
+        b: The second integer
+    Returns:
+        If an error occurs - addition of integers
+        otherwise - the desired result
+    '''
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return (result)
