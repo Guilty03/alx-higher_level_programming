bc643708a4f2af7889cff9106a0e152e078f5188
innocent <innocent04102003@gmail.com>
2024-02-21 10:53:28 -0800

    JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Retrieves the status of a GET request
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code:' + ' ' + response.statusCode);
+  }
+});
+
---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Retrieves content from a URL and saves it to a file
+
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
+
